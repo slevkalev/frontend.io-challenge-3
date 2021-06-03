92178d515c0840caaf8718eeab9a2aa4afc3b4d8
Jim Snelgrove <60110040+slevkalev@users.noreply.github.com>
2021-06-03 14:32:36 -0400

    calculator javascript complete
    all calculations work
    =reset and del work
    display size parameters need to be completed

--- main.js
+++ main.js
@@ -1,34 +1,90 @@
+//variables
+
 const keys = document.querySelectorAll('.key');
+const operators=["+","-","x","/"]
+const screen = document.querySelector('.screen')
+let reg1 = ""
+let reg2 = ""
+let op = ""
 
-const operators=["+","-","x","/","RESET", "=","DEL"]
 
+//Event Listeners
 
 keys.forEach(key=>{
 	key.addEventListener('click', register)
 })
 
-const screen = document.querySelector('.screen')
-let reg = ""
 
 
-function calculate(string){
+//functions
 
+//calculate results in a number based on 1st registered number (reg1) 2nd registered number (reg2) and 
+function calculate(reg1, reg2, op){
+	if(op=="+"){
+		return Number(reg1) + Number(reg2)
+	}
+	if(op=="-"){
+		return Number(reg2) - Number(reg1)
+	}
+	if(op=="x"){
+		return Number(reg1) * Number(reg2)
+	}
+	if(op=="/"){
+		return Number(reg2) / Number(reg1)
+	}
 }
 
+function display(reg){
+	screen.innerText=reg
+}
+
+
+// there are 2 number registers and an operator register to control the screen output
+// reg1 is the active register for each number keystroke
+// the DEL key only acts only on the active register
+// reg2 is set when the active register(reg1) is not empty and an operator keystroke has occurred.
 
 function register(e){
 	const value = e.target.querySelector('p').innerText
 		if(value == "RESET"){
-			reg=""
-			screen.innerText=""
+			reg1="" // clear register 1
+			reg2="" // clear register 2
+			op=""  // clear operator
+			display(reg1) // clear display
 		}else if(value=="DEL"){
-			reg = reg.slice(0, -1)
-			screen.innerText=reg
-		} else if(value=="="){
-			calculate(reg)
+			if(reg1==""){
+				display(reg2)
+			}else{
+				reg1 = reg1.slice(0,-1)
+				display(reg1)	
+			}
+		}else if(value=="="){
+			if(reg1 && reg2 && op){
+			reg2 = calculate(reg1, reg2, op)
+			reg1 = ""
+			op = ""
+			display(reg2)
+			}else if(reg2 && !reg1 && !op){
+				display(reg2)
+			}
+		}else if(operators.includes(value)){
+			if(reg2 && reg1){
+				reg2 = calculate(reg1, reg2, op)
+				display(reg2)
+				op=value
+				reg1=""
+			}else if(reg2 && !reg1){
+				op=value
+			}else{
+				reg2 = reg1
+				op= value
+				reg1=""
+			}
 		}else{
-			reg+=value
-			screen.innerText=reg
+			reg1+=value
+			console.log(reg1)
+			display(reg1)
 		}
 		
 }
+
